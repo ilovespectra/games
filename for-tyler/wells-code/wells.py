5f44c0a553f8e2653d8c418292562d6168f8baab
Tanner McMullen <94848716+ilovespectra@users.noreply.github.com>
2023-01-11 09:25:08 -0700

    Update wells.py

--- for-tyler/wells-code/wells.py
+++ for-tyler/wells-code/wells.py
@@ -6,5 +6,5 @@ def generate_response(file_path, width):
     wrapped_response = textwrap.fill(response, width=width)
     print("\033[47m\033[30m" + wrapped_response + "\033[0m")
 
-generate_response("wells2.txt", 94)
+generate_response("goggles.txt", 94)
 
