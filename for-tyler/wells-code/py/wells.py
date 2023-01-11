e6515a14fba1f7b47e0bd81edec33994a09d711e
Tanner McMullen <94848716+ilovespectra@users.noreply.github.com>
2023-01-11 12:40:46 -0700

    Update wells.py

--- for-tyler/wells-code/py/wells.py
+++ for-tyler/wells-code/py/wells.py
@@ -12,4 +12,4 @@ def generate_response(file_path, width, typing_speed):
         time.sleep(typing_speed)
     print("\033[0m")
 
-generate_response("goggles.txt", 94, 0.0002)
+generate_response("/txt/goggles.txt", 94, 0.0002)
