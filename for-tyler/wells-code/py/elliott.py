1f7e45cdb4c812d490abf1c1247c8f9548826edb
Tanner McMullen <94848716+ilovespectra@users.noreply.github.com>
2023-01-11 13:30:26 -0700

    Add files via upload

---
+++ for-tyler/wells-code/py/elliott.py
@@ -0,0 +1,15 @@
+import textwrap
+import time
+import sys
+
+def generate_response(file_path, width, typing_speed):
+    with open(file_path, "r") as f:
+        response = f.read()
+    wrapped_response = textwrap.fill(response, width=width)
+    for char in wrapped_response:
+        print("\033[47m\033[30m" + char, end='')
+        sys.stdout.flush()
+        time.sleep(typing_speed)
+    print("\033[0m")
+
+generate_response("../txt/elliott_300px.txt", 300, 0.0002)
