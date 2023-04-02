ae9398279bd9faeb1f02d737264e59b0c6c23d86
Tanner McMullen <94848716+ilovespectra@users.noreply.github.com>
2023-04-02 07:15:21 -0600

    Update coundown-push.py
    correct interval

--- coundown-push.py
+++ coundown-push.py
@@ -26,6 +26,7 @@ def send_notification(message):
     print(response.text)
 
 countdown_date =  time.mktime(time.strptime('April 18, 2023 10:00:00', '%B %d, %Y %H:%M:%S'))
+last_notification_time = 0
 
 while True:
     now = time.time()
@@ -35,26 +36,39 @@ while True:
         message = 'The Helium x Solana migration has begun!'
         send_notification(message)
         break
-    elif remaining_time > 86400:
-        # Send notification 24 hours before event
+    elif remaining_time > 86400 and (now - last_notification_time) >= 86400:
+        # Send notification once a day until 24 hours before event
         message = f'Just {int(remaining_time/86400)} days until the Solana migration!'
+        send_notification(message)
+        last_notification_time = now
     elif remaining_time > 43200:
         # Send notification 12 hours before event
         message = 'Just 12 hours until the Solana migration!'
+        send_notification(message)
+        time.sleep(600) # wait for 10 minutes before sending the next notification
     elif remaining_time > 21600:
         # Send notification 6 hours before event
         message = 'Just 6 hours until the Solana migration!'
+        send_notification(message)
+        time.sleep(600) # wait for 10 minutes before sending the next notification
     elif remaining_time > 10800:
         # Send notification 3 hours before event
         message = 'Just 3 hours until the Solana migration!'
+        send_notification(message)
+        time.sleep(600) # wait for 10 minutes before sending the next notification
     elif remaining_time > 7200:
         # Send notification 2 hours before event
         message = 'Just 2 hours until the Solana migration!'
+        send_notification(message)
+        time.sleep(600) # wait for 10 minutes before sending the next notification
     elif remaining_time > 3600:
         # Send notification 1 hour before event
         message = 'Just 1 hour until the Solana migration!'
+        send_notification(message)
+        time.sleep(600) # wait for 10 minutes before sending the next notification
     else:
         # Send notification every 10 minutes in the last hour
         message = 'The Solana migration is happening now!'
-    send_notification(message)
-    time.sleep(600) # wait for 10 minutes before sending the next notification
+        send_notification(message)
+        time.sleep(600) # wait for 10 minutes before sending the next notification
+
